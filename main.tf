b1b05e2a943c538a24700fda2738109710f9d4b0
Paul Bruffett <paulbruffett@protonmail.com>
2022-03-01 19:58:16 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -99,10 +99,10 @@ resource "google_dataflow_job" "big_data_job" {
   template_gcs_path = "gs://dataflow-templates/latest/PubSub_to_BigQuery"
   region = "us-central1"
   temp_gcs_location = google_storage_bucket.gcs-temp.url
-  additionalExperiments = ["enable_prime"]
+  additional_experiments = ["enable_prime"]
   parameters = {
-    outputTableSpec = google_bigquery_table.arduinoreadings.table_id
-    inputTopic = google_pubsub_topic.arduino-telemetry.name
+    output_table_spec = google_bigquery_table.arduinoreadings.table_id
+    input_topic = google_pubsub_topic.arduino-telemetry.name
   }
 }
 
